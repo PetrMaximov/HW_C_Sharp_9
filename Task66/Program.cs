1fc29766bbcf4a68100ff44aa98eb28093f2500e
«Petr» <maxpetrb@mail.ru>
2023-10-01 19:30:06 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Sum = 0;
+int SummNaturalNumbers(int M, int N)
+{
+    if (M < (N + 1)) 
+    {
+        Sum = M + SummNaturalNumbers(M + 1, N);
+        return Sum; 
+    }
+    else return Sum;
+}
+
+int res = SummNaturalNumbers(4, 8);
+Console.WriteLine(res);
